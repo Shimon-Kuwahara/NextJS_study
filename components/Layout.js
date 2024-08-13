80d1eb10c0f1e366eba466ca1e609d733972b250
Shimon-Kuwahara <tukubamj@icloud.com>
2024-08-13 16:27:36 +0900

    レイアウトの適応

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+
+const name = '桑原 士門'
+export const siteTitle = "Next.js blog"
+
+function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel='icon' href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <img src="vercel.svg" className={utilStyles.borderCircle}/>
+        <h1>{name}</h1>
+      </header>
+      <main>{ children }</main>
+    </div>
+  )
+}
+
+export default Layout
